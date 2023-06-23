3cc61eb41557969a219e7477ad18fbce027a410e
Ahemd <ahmedsaye23156@gmail.com>
2023-06-23 14:26:04 +0300

    play the blues

---
+++ app/page.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className='  bg-blue-900 w-screen h-screen flex  justify-center items-center'>
+        <div className="text-center w-full">
+          <button className=" px-4 bg-white p-2 rounded-md ">Login With Google</button>
+        </div>
+      </div>
+    </>
+  )
+}
